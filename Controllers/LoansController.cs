d09c21b7706ad89f34faa11a314df29e5f5e8335
Fateh Ahmado <ahmadofateh@gmail.com>
2026-07-21 21:20:24 +0200

    feat: show all devices in Create dropdown, validate availability on submit

--- Controllers/LoansController.cs
+++ Controllers/LoansController.cs
@@ -36,10 +36,11 @@ namespace Utlanssystem.Controllers
         // GET: Loans/Create - viser skjema med nedtrekkslister for ledige enheter og studenter uten lån.
         public IActionResult Create()
         {
-            var availableDevices = _context.Devices.Where(d => d.IsAvailable == true);
-            var availableStudents = _context.Students.Where(s => s.HasActiveLoan == false);
+            var allDevices = _context.Devices;
+            var availableStudents = _context.Students
+                .Where(s => _context.Loans.Count(l => l.StudentId == s.Id) < 2);
 
-            ViewBag.DeviceId = new SelectList(availableDevices, "Id", "Name");
+            ViewBag.DeviceId = new SelectList(allDevices, "Id", "Name");
             ViewBag.StudentId = new SelectList(availableStudents, "Id", "FirstName");
 
             return View();
@@ -53,21 +54,36 @@ namespace Utlanssystem.Controllers
             var device = await _context.Devices.FindAsync(loan.DeviceId);
             var student = await _context.Students.FindAsync(loan.StudentId);
 
-            if (device != null && student != null)
+            if (device == null || student == null)
             {
-                loan.LoanDate = DateTime.Now;
-
-                device.IsAvailable = false;
-                student.HasActiveLoan = true;
-
-                _context.Add(loan);
-                await _context.SaveChangesAsync();
-
-                TempData["SuccessMessage"] = $"Lån registrert: {student.FirstName} {student.LastName} har lånt {device.Name}!";
-                return RedirectToAction(nameof(Index));
+                TempData["ErrorMessage"] = "Ugyldig enhet eller student valgt.";
+                return RedirectToAction(nameof(Create));
             }
 
-            return RedirectToAction(nameof(Create));
+            // Forretningsregel 1: enheten må være tilgjengelig
+            if (!device.IsAvailable)
+            {
+                TempData["ErrorMessage"] = $"'{device.Name}' er allerede utlånt. Velg en annen enhet.";
+                return RedirectToAction(nameof(Create));
+            }
+
+            // Forretningsregel 2: studenten kan ha maks 2 aktive lån samtidig
+            int activeLoanCount = _context.Loans.Count(l => l.StudentId == student.Id);
+            if (activeLoanCount >= 2)
+            {
+                TempData["ErrorMessage"] = $"{student.FirstName} {student.LastName} har allerede 2 aktive lån (maks tillatt).";
+                return RedirectToAction(nameof(Create));
+            }
+
+            loan.LoanDate = DateTime.Now;
+            device.IsAvailable = false;
+            student.HasActiveLoan = true;
+
+            _context.Add(loan);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = $"Lån registrert: {student.FirstName} {student.LastName} har lånt {device.Name}!";
+            return RedirectToAction(nameof(Index));
         }
     }
     
