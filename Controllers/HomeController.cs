1a0bbde4ffeaedc71abe73a855e5559d2a8cf81c
Fateh Ahmado <ahmadofateh@gmail.com>
2026-07-21 23:25:46 +0200

    style: add dark theme, dashboard homepage, and improved form/table styling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Utlanssystem.Data;
 using Utlanssystem.Models;
 
 namespace Utlanssystem.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly UtlanssystemContext _context;
+
+    public HomeController(UtlanssystemContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
+        ViewBag.TotalDevices = _context.Devices.Count();
+        ViewBag.AvailableDevices = _context.Devices.Count(d => d.IsAvailable == true);
+        ViewBag.ActiveLoans = _context.Loans.Count();
+        ViewBag.TotalStudents = _context.Students.Count();
+        
         return View();
     }
 
